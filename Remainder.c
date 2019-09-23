f62cbc5a96984bca1c0195c22368ee030fc29d08
Hiren Vadher <hirenvadher954@gmail.com>
2019-09-23 23:50:40 +0530

    Create Remainder.c

---
+++ Remainder.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void){
+    int n;;
+    scanf("%d",&n);
+    while(n--){
+        int a,b;
+        scanf("%d %d",&a,&b);
+        printf("%d\n",a%b);   
+    }
+    return 0;
+};
