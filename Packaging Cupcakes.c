85f66668ba1b612bf2fdbba8e7f493824ac6cb2e
Hiren Vadher <hirenvadher954@gmail.com>
2019-09-25 19:47:47 +0530

    Create Packaging Cupcakes.c

---
+++ Packaging Cupcakes.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    while(n--){
+        int a;
+        scanf("%d",&a);
+        printf("%d\n",a/2+1);
+    }
+
+    return 0;
+}
