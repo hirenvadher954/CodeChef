5bbc957f6ee96648696ee83d9d33d0b6752abd72
Hiren Vadher <hirenvadher954@gmail.com>
2019-10-23 22:03:55 +0530

    Create Good Joke!.py

---
+++ Good Joke!.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for i in range(t):
+    a = int(input())
+    temp = 0
+    for i in range(1,a+1):
+        temp ^= i
+    for j in range(a):
+        b = input()
+    print(temp)
