97c56c908f2a7baa28d37e04a51631d9dc35317c
Hiren Vadher <hirenvadher954@gmail.com>
2019-10-26 19:20:34 +0530

    Create Bear and Candies 123.py

---
+++ Bear and Candies 123.py
@@ -0,0 +1,22 @@
+# cook your dish here
+for i in range(int(input())):
+    a,b=map(int,input().split())
+    limak=0
+    bob=0
+    k=3
+    for i in range(1,100000):
+        if(i%2==0):
+            bob=bob+i
+            if(bob>b):
+                k=0
+                break
+        else:
+            limak=limak+i
+            if(limak>a):
+                k=1
+                break
+    if(k==1):
+        print('Bob')
+    if(k==0):
+        print('Limak')
+            
