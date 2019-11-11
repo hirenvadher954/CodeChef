1765c057afb5d7aa59e6512e2ad1e5c60b230b94
Hiren Vadher <hirenvadher954@gmail.com>
2019-11-11 20:40:55 +0530

    Create A - Books.py

---
+++ A - Books.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n = int(input())
+    l = list(map(int,input().split()))
+    temp = []
+    for i in range(n):
+        c=0
+        for j in range(i+1,n):
+            if l[i]<l[j]:
+                c += 1
+        temp.append(c)
+    print(*temp)    
+    
