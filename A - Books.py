e6059155d4987ad8b03c997d1f9ff6e4a21a8edb
Hiren Vadher <hirenvadher954@gmail.com>
2020-02-13 23:59:31 +0530

    Update A - Books.py

--- A - Books.py
+++ A - Books.py
@@ -3,7 +3,7 @@ for t in range(int(input())):
     l=list(map(int,input().split()))
     temp = []
     for i in range(n):
-        c=0
+        c= 0
         for j in range(i+1,n):
             if l[i]<l[j]:
                 c += 1
